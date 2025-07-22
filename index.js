fef35c9d9d33164800ffd2ad7adfd4aeccdf92a0
mohamed5adel <addelmohamed286@gmail.com>
2025-07-23 02:18:22 +0300

    initial upload for hello-eyego project

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send(' Hello Eyego ');
+});
+
+app.get('/api/greet', (req, res) => {
+  res.send('Hello Eyego');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
